bfc7c47b99f642d6af9a47ad8ac70b59fbbca2b7
rishirajreddy <rishirajreddy.07@gmail.com>
2022-08-06 23:47:59 +0530

    modified db url

--- db/db.js
+++ db/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-// const url = "mongodb://localhost:27017/test";
-// const url = "mongodb+srv://mrrobot:mrrobot@empmngmt.cs734kp.mongodb.net/mini-insta?retryWrites=true&w=majority";
 
 const connectDB = () => {
     mongoose.connect(process.env.MONGO_URL,{
